9ba0f2c26e37b366759e6f69ce784ca427e35142
Matheus Grawe <matheus.grawe12@gmail.com>
2025-06-07 21:12:35 -0300

    feat(product): Add Product entity and create products table

---
+++ app/Entities/Product.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="products")
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
+    protected int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected string $name;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    protected string $description;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    protected float $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    protected string $category;
+
+    // --- Getters (para ler os dados) ---
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
+
+    // --- Setters (para alterar os dados) ---
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function setPrice(float $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function setCategory(string $category): void
+    {
+        $this->category = $category;
+    }
+}
